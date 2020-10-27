8c09de14f67d556f30723728d31bafed56f4650c
Ashwath <ashwathnarayana.r@echidnainc.com>
2020-10-27 18:28:19 +0530

    Print functionality

--- scripts/pages/subscription-confirmation.js
+++ scripts/pages/subscription-confirmation.js
@@ -319,3 +319,18 @@ require([
     });
 
 });
+
+function printform(){
+    var printpage = "page-content";
+    var headstr = "<html><head><title></title></head><body>";
+    var footstr = "</body>";
+    var newstr = document.all.item(printpage).innerHTML;
+    var oldstr = document.body.innerHTML;
+    document.body.innerHTML = headstr + newstr + footstr;
+    window.print();
+    setTimeout(function() {
+        document.body.innerHTML = oldstr;
+        return false;
+    },2000);
+    
+}
